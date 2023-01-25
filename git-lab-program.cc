a1bd2e91f825becc5f1eda565179ebdfd9fc8067
lainie-daniel <ladla2902@gmail.com>
2023-01-24 19:17:24 -0500

    First Program

---
+++ git-lab-program.cc
@@ -0,0 +1,22 @@
+/**
+ * @file git-lab-program.cc
+ * @author Lainie Daniel 
+ * @brief Using git to update repos
+ * @version 0.1
+ * @date 2023-01-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "Hello Git!" << endl;
+
+    return 0;
+} //main
